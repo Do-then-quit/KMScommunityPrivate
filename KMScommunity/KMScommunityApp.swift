ea4e3c9904e72fdf4873868533ad2ac3ec578ca8
MinkyoLee <redfowl@naver.com>
2022-12-30 00:52:02 +0900

    Initial Commit

---
+++ KMScommunity/KMScommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMScommunityApp.swift
+//  KMScommunity
+//
+//  Created by 이민교 on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct KMScommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
