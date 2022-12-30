687e664a78c16a9288d3812ea27d858dff392757
MinkyoLee <redfowl@naver.com>
2022-12-30 17:02:28 +0900

    [Feat] 회원가입을 위한 텍스트필드들, 회원가입기능

--- KMScommunity/ContentView.swift
+++ KMScommunity/ContentView.swift
@@ -6,18 +6,38 @@
 //
 
 import SwiftUI
-
+//{
+//  "email": "string",
+//  "name": "string",
+//  "nickname": "string",
+//  "phone": "string",
+//  "userId": "string",
+//  "userPw": "string"
+//}
 struct ContentView: View {
     @State private var userId: String = ""
-    @State private var password: String = ""
+    @State private var userPw: String = ""
+    @State private var email: String = ""
+    @State private var name: String = ""
+    @State private var nickname: String = ""
+    @State private var phone: String = ""
     
     var body: some View {
         VStack {
             TextField("UserID", text: $userId)
                 .border(.secondary)
             Button("중복 확인", action: idCheck)
-            SecureField("Password", text: $password)
+            SecureField("Password", text: $userPw)
                 .border(.secondary)
+            TextField("Email", text: $email)
+                .border(.secondary)
+            TextField("Name", text: $name)
+                .border(.secondary)
+            TextField("phone", text: $phone)
+                .border(.secondary)
+            TextField("nickname", text: $nickname)
+                .border(.secondary)
+            Button("회원가입", action: userRegister)
         }
         .padding()
     }
@@ -27,6 +47,9 @@ struct ContentView: View {
         requestGetWithQuery(url: "/user/idValidChk", inputID: userId) { (isCompletion: Bool, data: Any) in
             
         }
+    }
+    func userRegister() -> Void {
+        postUserRegister(userId: userId, userPw: userPw, email: email, name: name, phone: phone, nickname: nickname)
     }
 
 }
--- KMScommunity/request.swift
+++ KMScommunity/request.swift
@@ -13,6 +13,8 @@ struct Response: Codable {
     let message: String
 }
 
+
+
 let urlString = "http://35.89.32.201:8081"
 
 func requestGetWithQuery(url: String,inputID: String, completionHandler: @escaping (Bool, Any) -> Void) {
@@ -41,3 +43,54 @@ func requestGetWithQuery(url: String,inputID: String, completionHandler: @escapi
     }.resume()
 }
 
+func postUserRegister(userId: String, userPw: String, email: String, name: String, phone: String, nickname: String) {
+    guard var urlComponents = URLComponents(string: urlString + "/user/register") else {
+        print("Error: cannot create URL")
+        return
+    }
+    
+    let dicData = [
+        "userId": userId,
+        "userPw": userPw,
+        "email": email,
+        "name": name,
+        "phone": phone,
+        "nickname": nickname
+    ] as Dictionary<String, String>?
+    let jsonData = try! JSONSerialization.data(withJSONObject: dicData!, options: [])
+    let testjson = String(data: jsonData, encoding: .utf8) ?? ""
+    print(testjson)
+    
+    var requestURL = URLRequest(url: urlComponents.url!)
+    requestURL.httpMethod = "POST"
+    requestURL.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    requestURL.httpBody = jsonData
+    
+    let session = URLSession(configuration: .default)
+    
+    session.dataTask(with: requestURL) { (data: Data?, response: URLResponse?, error: Error?) in
+        guard error == nil else {
+            print("Error occur: error calling POST - \(String(describing: error))")
+            return
+        }
+
+        guard let data = data, let response = response as? HTTPURLResponse, (200..<300) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        
+        print(String(decoding: data, as: UTF8.self))
+        print(response)
+    }.resume()
+    
+    
+
+}
+//{
+//  "email": "string",
+//  "name": "string",
+//  "nickname": "string",
+//  "phone": "string",
+//  "userId": "string",
+//  "userPw": "string"
+//}
