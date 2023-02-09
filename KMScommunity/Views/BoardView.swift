25a86d3dad0643a2918b4a015b1c0daa77ccb1ae
MinkyoLee <redfowl@naver.com>
2023-02-09 16:34:48 +0900

    게시판 페이징 처리. 그 과정에서 게시물 추가 버튼 관련해서는 조금 대충 때운 감이 있다.

--- KMScommunity/Views/BoardView.swift
+++ KMScommunity/Views/BoardView.swift
@@ -20,6 +20,9 @@ struct BoardView: View {
     
     @State var isLoading = false
     
+    @State var curPage = 0
+    @State var isAddButtonSelect :Int? = nil
+    
     var body: some View {
         ZStack {
             VStack {
@@ -40,48 +43,97 @@ struct BoardView: View {
                         Text("Search")
                     }
                     .padding(.trailing)
-                    
-
+                }
+                // navigation to createboard
+                NavigationLink(destination: BoardCreateView(), tag: 1, selection: $isAddButtonSelect) {
+                    //Empty view
                 }
                 List{
+                    HStack {
+                        Text("현재 페이지 : \(curPage)")
+                        Spacer()
+                        
+                        Button("새글작성") {
+                            isAddButtonSelect = 1
+                            
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                     
                     ForEach(boardList.data) { board in
                         NavigationLink(destination: BoardDetailView(boardId: board.boardId)) {
                             BoardCardView(board: board)
-                                
                         }
+                    }
+                    
+                    
+                    
+                    HStack {
+                        if curPage > 0 {
+                            Button("이전") {
+                                curPage -= 1
+                                Task {
+                                    isLoading = true
+                                    print("boardview appeared?")
+                                    await boardList = getBoardList(page: curPage)
+                                    isLoading = false
+                                }
+                            }
+                            .buttonStyle(.borderedProminent)
+                        }
+                        Spacer()
+                        // 마지막 페이지인지는 아직 모르니까 그냥 냅두자.
+                        Button("다음") {
+                            curPage+=1
+                            Task {
+                                isLoading = true
+                                print("boardview appeared?")
+                                await boardList = getBoardList(page: curPage)
+                                isLoading = false
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                    HStack {
+                        Text("현재 페이지 : \(curPage)")
+                        Spacer()
+                            
                     }
                 }
                 
+                
+                // page 컨트롤
+                
+                
             }
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    NavigationLink(destination: BoardCreateView()) {
-                        Text("+")
-                            .font(.system(.largeTitle))
-                            .frame(width: 77, height: 70)
-                            .foregroundColor(Color.white)
-                            .padding(.bottom, 7)
-                    }
-                    .background(Color.blue)
-                    .cornerRadius(38.5)
-                    .padding()
-                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
-
-                    
-
-                }
-            }
+            // 아래는 그 플로팅 +
+//            VStack {
+//                Spacer()
+//                HStack {
+//                    Spacer()
+//                    NavigationLink(destination: BoardCreateView()) {
+//                        Text("+")
+//                            .font(.system(.largeTitle))
+//                            .frame(width: 77, height: 70)
+//                            .foregroundColor(Color.white)
+//                            .padding(.bottom, 7)
+//                    }
+//                    .background(Color.blue)
+//                    .cornerRadius(38.5)
+//                    .padding()
+//                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
+//                }
+//            }
             if isLoading {
                 ProgressView()
             }
+            
             
         }
         .task {
             isLoading = true
             print("boardview appeared?")
-            await boardList = getBoardList()
+            await boardList = getBoardList(page: curPage)
             isLoading = false
         }
     }
